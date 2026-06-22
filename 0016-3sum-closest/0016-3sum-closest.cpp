08e4920eebb8ea04819af24e81b611af8fa16285
abhinav101010 <71586575+abhinav101010@users.noreply.github.com>
2026-06-22 10:40:19 +0530

    Time: 873 ms (5%), Space: 14.1 MB (37.53%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int diffNum = nums[0] + nums[1] + nums[2];
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                for(int k=j+1;k<nums.size();k++){
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(abs(target - sum) < abs(target - diffNum)) {
+                        diffNum = sum;
+                    }
+                
+                }
+            }
+        }
+
+        return diffNum;
+    }
+};
