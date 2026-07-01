5c89d26905ecd5bca549962f430a79f8fde3796f
Abhinav <71586575+abhinav101010@users.noreply.github.com>
2026-07-01 07:17:59 +0530

    Time: 375 ms (72.26%), Space: 19.4 MB (46.21%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,10 +1,36 @@
+# class Solution:
+#     def threeSumClosest(self, nums: List[int], target: int) -> int:
+#         ans = nums[0]+nums[1]+nums[2]
+#         for i in range(0,len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 for k in range(j+1,len(nums)):
+#                     s = nums[i]+nums[j]+nums[k]
+#                     if abs(s-target) < abs(ans-target):
+#                         ans = s
+#                     if abs(s-target) == 0: return s
+#         return ans
+        
+
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        ans = 2**31
-        for i in range(len(nums)):
-            for j in range(i,len(nums)):
-                for k in range(j,len(nums)):
-                    if ((nums[i]+nums[j]+nums[k])-target) < ans:
-                        ans = nums[i]+nums[j]+nums[k]
-        return ans
-        
+        nums.sort()
+        closest = nums[0] + nums[1] + nums[2]
+
+        for i in range(len(nums) - 2):
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                curr = nums[i] + nums[left] + nums[right]
+
+                if abs(curr - target) < abs(closest - target):
+                    closest = curr
+
+                if curr < target:
+                    left += 1
+                elif curr > target:
+                    right -= 1
+                else:
+                    return curr
+
+        return closest
