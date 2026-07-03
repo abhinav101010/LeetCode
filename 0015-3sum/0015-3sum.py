eb07054783e2f223886e88d578bfaf05cb03e26a
Abhinav <71586575+abhinav101010@users.noreply.github.com>
2026-07-03 07:03:26 +0530

    Time: 2609 ms (5%), Space: 22.2 MB (81.22%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -22,11 +22,11 @@ class Solution:
                     right-=1
                 elif s < 0:
                     left+=1
-                    # while left < right and nums[left] == nums[left - 1]:
-                    #     left += 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
                 elif s > 0:
                     right-=1
-                    # while left < right and nums[right] == nums[right + 1]:
-                    #     right -= 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
 
         return list(ans)
