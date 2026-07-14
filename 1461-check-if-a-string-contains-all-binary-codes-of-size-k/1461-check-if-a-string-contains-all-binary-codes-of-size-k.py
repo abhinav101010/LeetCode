49f458fdea35d20a36d8b3cdd5d3c4cadcd0d98b
Abhinav <71586575+abhinav101010@users.noreply.github.com>
2026-07-14 07:06:34 +0530

    Time: 324 ms (22.02%), Space: 65.1 MB (5.2%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -0,0 +1,43 @@
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+# Tried through subsets: ----
+        # ans = True
+        # def checkCode(s, curr, i):
+        #     nonlocal ans
+        #     if i == k:
+        #         if curr not in s:
+        #             ans = False
+        #         return
+
+        #     if ans:
+        #         checkCode(s, curr+"0", i+1)
+        #         checkCode(s, curr+"1", i+1)
+        
+        # checkCode(s, "", 0)
+        # return ans
+
+# Tried through Permutation :----
+        # ans = True
+        # def generate(bits, start):
+        #     nonlocal ans
+        #     if not ans:
+        #         return
+
+        #     if start == len(bits):
+        #         if "".join(bits) not in s:
+        #             ans = False
+        #         return
+
+        #     for c in ("0", "1"):
+        #         bits[start] = c
+        #         generate(bits, start + 1)
+        # generate([""] * k, 0)
+        # return ans
+
+
+        subStrs = set()
+        for i in range(len(s)-k+1):
+            subStrs.add(s[i:i+k])
+        print(subStrs)
+
+        return len(subStrs) == 2**k
