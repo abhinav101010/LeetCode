1d739c34be2e05f3a885cd0e42c8a05b62b8169e
Abhinav <71586575+abhinav101010@users.noreply.github.com>
2026-07-15 06:49:07 +0530

    Time: 0 ms (100%), Space: 19.2 MB (53.03%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -1,6 +1,9 @@
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
-        sumOdd = sum(i for i in range(1,2*n,2))
-        sumEven = sum(i for i in range(2,(2*n)+1,2))
+        # sumOdd = sum(i for i in range(1,2*n,2))
+        # sumEven = sum(i for i in range(2,(2*n)+1,2))
 
-        return gcd(sumOdd, sumEven)
+        # return gcd(sumOdd, sumEven)
+
+# i didnt knew gcd will be same as n, the approch above was thought by me
+        return n
