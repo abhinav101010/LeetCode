1a8c2859f5e66324759cf286c4ca7d6da2533017
Abhinav <71586575+abhinav101010@users.noreply.github.com>
2026-07-17 07:30:53 +0530

    Time: 52 ms (97.24%), Space: 21.6 MB (22.55%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -1,11 +1,19 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        ans = {}
+        # ans = {}
 
-        for i in range(len(word)):
-            upperChar = word.find(word[i].upper())
-            if word[i].islower() and upperChar > i:
-                ans.update({word[i]: upperChar})
-            if word[i] in ans.keys() and upperChar < i:
-                ans.pop(word[i])
-        return len(ans)
+        # for i in range(len(word)):
+        #     upperChar = word.find(word[i].upper())
+        #     if word[i].islower() and upperChar > i:
+        #         ans.update({word[i]: upperChar})
+        #     if word[i] in ans.keys() and upperChar < i:
+        #         ans.pop(word[i])
+        # return len(ans)
+
+# Both approch written by self, just didnt kenw of .rfind method
+        ans = 0
+        for char in "abcdefghijklmnopqrstuvwxyz":
+            if char in word and char.upper() in word:
+                if word.rfind(char) < word.find(char.upper()):
+                    ans+=1
+        return ans
