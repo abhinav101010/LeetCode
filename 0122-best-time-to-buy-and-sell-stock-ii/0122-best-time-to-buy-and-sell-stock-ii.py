7cb839bdce15b4ac3e82aa7474d40a45e6bd5856
Abhinav <71586575+abhinav101010@users.noreply.github.com>
2026-07-26 07:11:30 +0530

    Time: 0 ms (100%), Space: 20.3 MB (85.41%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        bought = False
+        n = len(prices)
+        for i in range(n):
+            if i < n-1:
+                if prices[i] < prices[i+1] and not bought:
+                    bought = True
+                    maxProfit -= prices[i]
+                    continue
+                
+                if prices[i] > prices[i+1] and bought:
+                    bought = False
+                    maxProfit += prices[i]
+                    continue
+                    
+            if bought and i == n-1:
+                bought = False
+                maxProfit += prices[i]
+
+        return maxProfit
