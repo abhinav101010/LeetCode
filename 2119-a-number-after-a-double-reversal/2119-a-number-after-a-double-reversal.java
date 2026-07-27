3c821018296a8ae6426cb013a3105bb94a4508eb
Abhinav <71586575+abhinav101010@users.noreply.github.com>
2026-07-27 15:16:48 +0530

    Time: 0 ms (100%), Space: 42.4 MB (11.92%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -1,11 +1,5 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-        String reversed = new StringBuilder(String.valueOf(num)).reverse().toString();
-        int reversedInt = Integer.valueOf(reversed);
-
-        reversed = new StringBuilder(String.valueOf(reversedInt)).reverse().toString();
-        reversedInt = Integer.valueOf(reversed);
-
-        return reversedInt == num;
+        return num == 0 || num % 10 != 0;
     }
 }
