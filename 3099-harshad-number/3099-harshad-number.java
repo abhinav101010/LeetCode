7d3d0e52e68e27981820231126883e895efc94e8
Abhinav <71586575+abhinav101010@users.noreply.github.com>
2026-07-27 15:34:44 +0530

    Time: 0 ms (100%), Space: 42 MB (81.18%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.java
+++ 3099-harshad-number/3099-harshad-number.java
@@ -1,13 +1,22 @@
 class Solution {
     public int sumOfTheDigitsOfHarshadNumber(int x) {
-        int sum = 0;
-        String str = String.valueOf(x);
-        for (int i = 0; i < str.length(); i++) {
-            sum += Integer.valueOf(String.valueOf(str.charAt(i)));
+        // int sum = 0;
+        // String str = String.valueOf(x);
+        // for (int i = 0; i < str.length(); i++) {
+        //     sum += Integer.valueOf(String.valueOf(str.charAt(i)));
+        // }
+        // if(x%sum == 0){
+        //     return sum;
+        // }
+        // return -1;
+
+        int sum = 0, temp = x;
+
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
         }
-        if(x%sum == 0){
-            return sum;
-        }
-        return -1;
+
+        return x % sum == 0 ? sum : -1;
     }
 }
