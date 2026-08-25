d27cf7e61039524c6fadeeac8264c690717379e4
Abhinav <71586575+abhinav101010@users.noreply.github.com>
2026-08-25 09:53:30 +0530

    Time: 1 ms (84.9%), Space: 22.5 MB (33.63%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -6,16 +6,28 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        nums = []
-        def dfs(root):
-            if not root: return
-            nums.append(root.val)
-            dfs(root.left)
-            dfs(root.right)
-        dfs(root)
+    # Self thought and worked, but learnt a new way
+        # nums = []
+        # def dfs(root):
+        #     if not root: return
+        #     nums.append(root.val)
+        #     dfs(root.left)
+        #     dfs(root.right)
+        # dfs(root)
         
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == k:
-                    return True
-        return False
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i]+nums[j] == k:
+        #             return True
+        # return False
+
+
+        seen=set()
+        def dfs(node):
+            if node is None:
+                return False
+            if k-node.val in seen:
+                return True
+            seen.add(node.val)
+            return dfs(node.left) or dfs(node.right)
+        return dfs(root)
